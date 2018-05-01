057d00821cc55ed8ab1818a9b8bb0a7a83435db5
Briana Table Pinckney <briep91@gmail.com>
2018-05-01 15:31:23 -0400

    Added function to app controller and console.log to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-	
 
+	require 'net/http'
+  require 'json'
+  def azavea_api_response
+    # encode URI using the URI constant
+    uri = URI("https://raw.githubusercontent.com/azavea/geo-data/master/Neighborhoods_Philadelphia/Neighborhoods_Philadelphia.geojson")
+    p uri
+    # tell Net::HTTP to GET the URI
+    # @response = JSON.parse(Net::HTTP.get(uri))
+    @response = Net::HTTP.get(uri)
+  end
 
 end
