9db3d0867acc65a153288e5d32cfad07b4c102fc
k <keisangittens@gmail.com>
2018-05-03 23:51:46 -0400

    printing info from map onto the DOM, got to the object but not quite there yet

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,6 @@ class MapController < ApplicationController
   def index
     @nei_response = azavea_api_response
     @counsel_response = counseling_api_response
-    @parks_response = parks_api_response
   end
 
   def show
