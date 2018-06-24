8a25fd028c40080cb91d95ee217a035537d1c7ab
Michael Lamb <mflambjr@gmail.com>
2018-06-24 02:14:57 -0400

    Remove search functionality from /search and search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
     def results
-        @realprop = Rubillow::PropertyDetails.deep_search_results({ :address => '2801 Jasper St', :citystatezip => 'Philadelphia, PA' })
-        @hood = Rubillow::Neighborhood.demographics({ :state => 'PA', :city => 'Philadelphia', :neighborhood => @realprop.region })
-        @comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @realprop.zpid, :count => 4 })
+        # @realprop = Rubillow::PropertyDetails.deep_search_results({ :address => '2801 Jasper St', :citystatezip => 'Philadelphia, PA' })
+        # @hood = Rubillow::Neighborhood.demographics({ :state => 'PA', :city => 'Philadelphia', :neighborhood => @realprop.region })
+        # @comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @realprop.zpid, :count => 4 })
     end
 end
