aaef83ff4b8f9605bf33ed36b1a61422447cd4d8
Michael Lamb <mflambjr@gmail.com>
2018-06-24 02:15:18 -0400

    Put search controller contents in map controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,7 +3,9 @@ class MapController < ApplicationController
   
 
   def index
- 
+    @realprop = Rubillow::PropertyDetails.deep_search_results({ :address => '2801 Jasper St', :citystatezip => 'Philadelphia, PA' })
+    @hood = Rubillow::Neighborhood.demographics({ :state => 'PA', :city => 'Philadelphia', :neighborhood => @realprop.region })
+    @comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @realprop.zpid, :count => 4 })
   end
 
   def show
