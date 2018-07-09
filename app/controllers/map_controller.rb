ce40b739f087e73c768a7fe181e1aad82fced3d7
Michael Lamb <mflambjr@gmail.com>
2018-07-08 20:01:25 -0400

    Remove commented out 'results' method that we're not currently using for ajax

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -14,22 +14,6 @@ class MapController < ApplicationController
     @comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @zillow.zpid, :count => 4 })
   end
 
-  # def results
-  #   if params[:address].present?
-  #     @string = "#{params[:address]}"
-  #   else
-  #     @string = "2810 Jasper St"
-  #   end
-  #   @zillow = Rubillow::PropertyDetails.deep_search_results({ :address => @string, :citystatezip => 'Philadelphia, PA' })
-  #   # @realprop = Rubillow::PropertyDetails.deep_search_results({ :address => '2801 Jasper St', :citystatezip => 'Philadelphia, PA' })
-  #   @hood = Rubillow::Neighborhood.demographics({ :state => 'PA', :city => 'Philadelphia', :neighborhood => @zillow.region })
-  #   @comps = Rubillow::PropertyDetails.deep_comps({ :zpid => @zillow.zpid, :count => 4 })
-  #   respond_to do |format|
-  #     format.html
-  #     format.json
-  #   end
-  # end
-
   def show
   	
   end
